23a7698f15a4cc895e8d77bd0a313fce5c9b51b6
Simon Lau <simon.lau.sg@gmail.com>
2026-07-12 17:42:53 +0800

    wip: ex02

---
+++ ex02/ft_ultimate_range.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: simon.lau <simon.lau@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/12 16:48:01 by simon.lau         #+#    #+#             */
+/*   Updated: 2026/07/12 17:07:11 by simon.lau        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	i;
+
+	if (max < min)
+	{
+		range = NULL;
+		return (0);
+	}
+	*range = malloc(sizeof(int));
+	if (!*range)
+	{
+		return (-1);
+	}
+	i = 0;
+	while (i < max - min)
+	{
+		*range[i] = min + i;
+		i++;
+	}
+	return (max - min);
+}
---
+++ ex02/test.c
@@ -0,0 +1,48 @@
+#include <assert.h>
+#include <stddef.h>
+
+int		ft_ultimate_range(int **range, int min, int max);
+
+void	test_invalid(void)
+{
+	int	*range;
+
+	assert(0 == ft_ultimate_range(&range, 10, 9));
+	assert(NULL == range);
+}
+
+// void	test_small(void)
+// {
+// 	int	*result;
+// 	int	min;
+// 	int	max;
+
+// 	min = 0;
+// 	max = 2;
+// 	result = ft_range(min, max);
+// 	assert(NULL != result);
+// 	for (int i = 0; i < max - min; i++)
+// 	{
+// 		assert(result[i] == min + i);
+// 	}
+// }
+
+// void	test_same(void)
+// {
+// 	int	*result;
+// 	int	min;
+// 	int	max;
+
+// 	min = 9;
+// 	max = min;
+// 	result = ft_range(min, max);
+// 	assert(NULL != result);
+// 	assert(result[0] == min);
+// }
+
+int	main(void)
+{
+	test_invalid();
+	// test_small();
+	// test_same();
+}
