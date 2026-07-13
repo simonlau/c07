cd2552bf3af64504cceb8492b2aaabb28367d3c7
Simon Lau <simon.lau.sg@gmail.com>
2026-07-13 13:29:02 +0800

    feat: did ex03

---
+++ ex03/ft_strjoin.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: simon.lau <simon.lau@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/13 10:29:08 by simon.lau         #+#    #+#             */
+/*   Updated: 2026/07/13 13:27:26 by simon.lau        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	str_len(char *str)
+{
+	int	count;
+
+	if (str == NULL)
+	{
+		return (0);
+	}
+	count = 0;
+	while (!str)
+	{
+		count++;
+	}
+	return (count);
+}
+
+int	calc_total_size(int size, char **str, char *sep)
+{
+	int	total;
+	int	i;
+
+	if (size == 0)
+	{
+		return (0);
+	}
+	total = 0;
+	i = 0;
+	while (i < size)
+	{
+		total += str_len(str[i]);
+		i++;
+	}
+	return (1 + total + str_len(sep) * (size - 1));
+}
+
+char	*move_to_str_end(char *str)
+{
+	char	*letter_ptr;
+
+	letter_ptr = str;
+	while (*letter_ptr != '\0')
+	{
+		letter_ptr++;
+	}
+	return (letter_ptr);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	char	*dest_end;
+	char	*letter_ptr;
+
+	letter_ptr = src;
+	if (letter_ptr == NULL)
+	{
+		return (dest);
+	}
+	dest_end = move_to_str_end(dest);
+	while (*letter_ptr != '\0')
+	{
+		*dest_end = *letter_ptr;
+		dest_end++;
+		letter_ptr++;
+	}
+	*dest_end = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(int size, char **str, char *sep)
+{
+	char	*result;
+	int		total_size;
+	int		i;
+
+	total_size = calc_total_size(size, str, sep);
+	result = malloc(total_size * sizeof(char));
+	if (!result)
+	{
+		return (NULL);
+	}
+	result[0] = '\0';
+	if (size == 0)
+	{
+		return (result);
+	}
+	i = 0;
+	while (i < size)
+	{
+		result = ft_strcat(result, str[i]);
+		if (i < size - 1)
+		{
+			result = ft_strcat(result, sep);
+		}
+		i++;
+	}
+	return (result);
+}
---
+++ ex03/test.c
@@ -0,0 +1,53 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char	*ft_strjoin(int size, char **str, char *sep);
+
+void	test_empty(void)
+{
+	char	*result;
+
+	result = ft_strjoin(0, NULL, NULL);
+	assert(0 == strcmp(result, ""));
+	free(result);
+}
+
+void	test_simple(void)
+{
+	char	*result;
+	char	*names[] = {"ab", "cd", "ef"};
+
+	result = ft_strjoin(3, names, "+-");
+	assert(0 == strcmp(result, "ab+-cd+-ef"));
+	free(result);
+}
+
+void	test_one(void)
+{
+	char	*result;
+	char	*names[] = {"X"};
+
+	result = ft_strjoin(1, names, NULL);
+	assert(0 == strcmp(result, "X"));
+	free(result);
+}
+
+void	test_two(void)
+{
+	char	*result;
+	char	*names[] = {"ab", "cd"};
+
+	result = ft_strjoin(2, names, "@");
+	assert(0 == strcmp(result, "ab@cd"));
+	free(result);
+}
+
+int	main(void)
+{
+	test_empty();
+	test_simple();
+	test_one();
+	test_two();
+}
